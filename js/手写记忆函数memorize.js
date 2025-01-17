0182dc538b46828db2304faa6f975dfa636f9689
Sumrian Shi <3092787775@qq.com>
2025-01-17 16:53:59 +0800

    perf: 优化记忆函数

--- js/手写记忆函数memorize.js
+++ js/手写记忆函数memorize.js
@@ -10,3 +10,27 @@ function memorize(fn) {
   }
 
 //   考虑返回函数的入参里面可能有多函数用剩余参数接受比较稳妥
+
+// 双map可以处理引用类型数据，但是会有内存泄漏的风险
+function memoize(fn) {
+    const argMap = new Map();
+    const argsMap = new Map();
+    let id = 0;
+    return function (...args) {
+        let key = "";
+        for (let arg of args) {
+            !argMap.has(arg) && argMap.set(arg, id++);
+            key += "-" + argMap.get(arg);
+        }
+        console.log(key);
+
+        if (!argsMap.has(key)) {
+            console.log("running");
+            const res = fn(...args);
+            argsMap.set(key, res);
+            return res;
+        } else {
+            return argsMap.get(key);
+        }
+    };
+}
